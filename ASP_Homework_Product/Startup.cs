00cf82b761c07c76c0091be4135310fe3ccfe672
George Gussaov <gussaov04@gmail.com>
2024-04-11 20:13:08 +0300

    Добавил логирование

--- ASP_Homework_Product/Startup.cs
+++ ASP_Homework_Product/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace ASP_Homework_Product
 {
@@ -44,6 +45,8 @@ namespace ASP_Homework_Product
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseSerilogRequestLogging();
 
             app.UseAuthorization();
 
